fbda080b606fb674accbf7932ee9c17ed48dc0eb
AMIRIOX <wumingyun2120@outlook.com>
2020-08-16 21:27:03 +0800

    第一次练习 良好

--- bigIntPrac.cpp
+++ bigIntPrac.cpp
@@ -1,3 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define maxn 500
+
+struct bigint {
+    int len, val[maxn];
+    bigint(int x = 0) {
+        memset(val, 0, sizeof val);
+        for (len = 1; x; len++) val[len] = x % 10, x /= 10;
+        len--;
+    }
+    int& operator[](int ind) { return val[ind]; }
+    void flatten(int L) {
+        len = L;
+        for (int i = 1; i <= L; i++) {
+            val[i + 1] += val[i] / 10, val[i] %= 10;
+        }
+        while (!val[len]) len--;
+    }
+    friend bigint operator+(bigint a, bigint b) {
+        bigint tmp;
+        int tlen = max(a.len, b.len);
+        for (int i = 1; i <= tlen; i++) {
+            tmp[i] += a[i] + b[i];
+        }
+        tmp.flatten(tlen + 1);
+        return tmp;
+    }
+    friend bigint operator*(bigint a, int c) {
+        int tlen = a.len;
+        bigint tmp;
+        for (int i = 1; i <= tlen; i++) {
+            tmp[i] = a[i] * c;
+        }
+        tmp.flatten(tlen + 11);
+        return tmp;
+    }
+    void print() {
+        for (int i = max(len, 1); i >= 1; i--) {
+            cout << val[i];
+        }
+    }
+};
+
+int main() {
+    /* code */
+    bigint a(2000000000);
+    bigint b(2000000000);
+    (a+b).print();
+    std::cout << std::endl;
+    (a*3).print();
+    return 0;
+}
