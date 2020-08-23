4e34c238b70f32194b7142dfb36095497a4ac06d
AMIRIOX <wumingyun2120@outlook.com>
2020-08-23 15:33:30 +0800

    AC LGR-075 Div2 T1

--- rates/LGR-075/T1.cpp
+++ rates/LGR-075/T1.cpp
@@ -22,27 +22,29 @@
 //     cout << lowCount << endl;
 //     return 0;
 // }
-#include <iostream>
 #include <cmath>
+#include <iostream>
 #define int long long
 using namespace std;
-int a,b,c;
-signed main(int argc, char const *argv[]) {
-	cin >> a >> b >> c;
-	if(c>=abs(a-b)) {
-		a>b?b+=abs(a-b):a+=abs(a-b);
-		c-=abs(a-b);
-	}else{
-		a>b?b+=c:a+=c;
-		c=0;
-	}
-	a+=c/2; b+=c/2; c/=2;
-	register int ans = 0;
-	// cout << a << ' ' << b << endl;
-	while(a>=ans+1 && b>=ans+1) {
-		ans++;
-		a-=ans; b-=ans;
-	}
-	cout << ans << endl;
-	return 0;
+int a, b, c;
+signed main(signed argc, char const *argv[]) {
+    cin >> a >> b >> c;
+    if (c >= abs(a - b)) {
+        c -= abs(a - b);
+        a > b ? b = a : a = b;
+    } else {
+        a > b ? b += c : a += c;
+        c = 0;
+    }
+    a += c / 2;
+    b += c / 2;
+    register int ans = 0;
+    // cout << a << ' ' << b << endl;
+    while (a >= ans + 1 && b >= ans + 1) {
+        ans++;
+        a -= ans;
+        b -= ans;
+    }
+    cout << ans << endl;
+    return 0;
 }
--- rates/LGR-075/rt2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdio>
-using namespace std;
-const int maxn = 100010;
-
-int main() {
-
-    return 0;
-}
