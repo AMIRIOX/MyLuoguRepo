4da08cfa899745a986dcaced905c398b966a97b9
AMIRIOX <wumingyun2120@outlook.com>
2020-09-13 20:16:06 +0800

    P1241 假设每个字符都需要配对, 遇到对应字符取消假设, 遍历后仍在假设需要配对的字符则需要输出对应的字符

--- P1241.cpp
+++ P1241.cpp
@@ -3,26 +3,43 @@
 #include <stack>
 #define int long long
 using namespace std;
-const int maxn = 1e5 + 10;
+const int maxn = 1e3 + 10;
 string x;
+char pls[maxn];
+stack<char> s;
+bool isLeft(char a) {
+    if (a == '(' || a == '[') return true;
+    return false;
+}
+bool isRight(char a) {
+    if (a == ')' || a == ']') return true;
+    return false;
+}
 char trans(char a) {
     if (a == '(') return ')';
     if (a == '[') return ']';
-    // if (a == '{') return '}';
-    return a;
+    if (a == ')') return '(';
+    if (a == ']') return '[';
 }
 signed main() {
     cin >> x;
-    stack<char> s;
     for (int i = 0; i < x.length(); i++) {
-        if (!s.empty() && x[i] == trans(s.top()))
-            s.pop();
-        else
-            s.push(x[i]);
+        if (isLeft(x[i])) {
+            s.push(i);
+            pls[i] = trans(x[i]);
+        } else {
+            if (s.empty() || pls[s.top()] != x[i]) {
+                pls[i] = trans(x[i]);
+            } else {
+                pls[s.top()] = '$';
+                s.pop();
+            }
+        }
     }
-    while (!s.empty()) {
-        cout << s.top();
-        s.pop();
+    for (int i = 0; i < x.length(); i++) {
+        if (isLeft(pls[i])) printf("%c", pls[i]);
+        cout << x[i];
+        if (isRight(pls[i])) printf("%c", pls[i]);
     }
     return 0;
 }
