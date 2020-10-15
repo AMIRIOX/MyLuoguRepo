e01dcec1e9f7740c6c6b6a652a9b6a729a7e1676
AMIRIOX <wumingyun2120@outlook.com>
2020-10-15 19:29:55 +0800

    P1309, ACed, 归并排序, 一个已排序的队员中两两比赛, 分出的win和lose也是有序

--- P1309.cpp
+++ P1309.cpp
@@ -1,93 +1,62 @@
+#include <algorithm>
 #include <cstdio>
 #include <iostream>
 #define int long long
 using namespace std;
 const int maxn = 1e6 + 1;
-int N, R, Q;
-inline int read() {
-    register int flag = 0, abs = 0;
-    char x = getchar();
-    while (x < '0' || x > '9') {
-        if (x == '-') flag = 1;
-        x = getchar();
-    }
-    while (x >= '0' && x <= '9') {
-        abs = (abs << 3) + (abs << 1) + (x ^ 48);
-        x = getchar();
-    }
-    return flag ? ~(abs - 1) : abs;
-}
 struct dy {
-    int score;
-    int abi;
-    int index;
-    dy() { score = 0, abi = 0, index = 0; }
-    dy(const dy& d) {
-        this->score = d.score;
-        this->abi = d.abi;
-        this->index = d.index;
+    int ind, score, abi;
+    dy() {}
+    dy(const dy& o) {
+        ind = o.ind;
+        score = o.score;
+        abi = o.abi;
     }
-    dy& operator=(const dy& o) {
-        this->abi = o.abi;
-        this->score = o.score;
-        this->index = o.index;
-        return *this;
-    }
-    bool operator>(dy& x) {
-        if(score!=x.score) return score>x.score;
-        else return index<x.index;
-    }
-} a[maxn], temp[maxn];
-inline void merge(int L, int mid, int R) {
-    register int i = L, j = mid + 1, cnt = 1;
-    while (i <= mid && j <= R) {
-        if (a[i]>a[j]) {
-            temp[cnt++] = a[i++];
-        }
-        if (a[j]>a[i]) {
-            temp[cnt++] = a[j++];
-        }
-    }
-    while (i <= mid) {
-        temp[cnt++] = a[i++];
-    }
-    while (j <= R) {
-        temp[cnt++] = a[j++];
-    }
-    cnt = 1;
-    while (L <= R) {
-        a[L++] = temp[cnt++];
-    }
+} a[maxn], win[maxn], lose[maxn];
+bool cmp(const dy& a, const dy& b) {
+    if (a.score != b.score)
+        return a.score > b.score;
+    else
+        return a.ind < b.ind;
 }
-inline void mergeSort(int L, int R) {
-    if (L < R) {
-        int mid = (L + R) / 2;
-        mergeSort(L, mid);
-        mergeSort(mid + 1, R);
-        merge(L, mid, R);
+void merge() {
+    int i = 1, j = 1;
+    int cnt = 0;
+    while (i <= win[0].ind && j <= lose[0].ind) {
+        if (cmp(win[i], lose[j])) {
+            a[++cnt] = win[i++];
+        } else {
+            a[++cnt] = lose[j++];
+        }
     }
+    while (i <= win[0].ind) a[++cnt] = win[i++];
+    while (j <= lose[0].ind) a[++cnt] = lose[j++];
 }
 signed main() {
+    int N, R, Q;
     cin >> N >> R >> Q;
-    for (register int i = 1; i <= 2 * N; i++) {
-        a[i].score = read();
-        a[i].index = i;
+    for (int i = 1; i <= 2 * N; i++) {
+        cin >> a[i].score;
+        a[i].ind = i;
     }
-    for (register int i = 1; i <= 2 * N; i++) {
-        a[i].abi = read();
-    }
-    for (register int i = 1; i <= R; i++) {
-        mergeSort(1, 2 * N);
-        for (register int j = 1; j <= 2 * N; j += 2) {
-            // a[i] a[i+1]
+    for (int i = 1; i <= 2 * N; i++) cin >> a[i].abi;
+    sort(a + 1, a + (N * 2) + 1, cmp);
+    for (int i = 1; i <= R; i++) {
+        win[0].ind = 0;
+        lose[0].ind = 0;
+        for (int j = 1; j <= 2 * N; j += 2) {
             if (a[j].abi > a[j + 1].abi) {
-                (a[j].score)++;
+                a[j].score++;
+                win[++(win[0].ind)] = a[j];
+                lose[++(lose[0].ind)] = a[j + 1];
             } else {
-                (a[j + 1].score)++;
+                a[j + 1].score++;
+                win[++(win[0].ind)] = a[j + 1];
+                lose[++(lose[0].ind)] = a[j];
             }
         }
+        merge();
     }
-    mergeSort(1, 2 * N);
-    cout << a[Q].index << endl;
+    cout << a[Q].ind << endl;
     return 0;
 }
