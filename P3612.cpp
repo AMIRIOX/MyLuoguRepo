e37b741e44334361a3cfb45fe3077dd23c808fab
AMIRIOX <wumingyun2120@outlook.com>
2020-10-17 23:09:27 +0800

    P3612 is ok. 模拟+推式子+细节

--- P3612.cpp
+++ P3612.cpp
@@ -1,22 +1,22 @@
-#include <algorithm>
 #include <cstdio>
 #include <iostream>
-#include <string>
 #define int long long
 using namespace std;
 const int maxn = 1e6 + 1;
 string x;
-int n;
+int q, n, i;
 signed main() {
-    cin >> x >> n;
-    int maxnn = n / (x.length()) + 1;
-    for (size_t i = 1; i < maxnn; i++) {
-        string t = x;
-        t.insert(t.begin(), x[x.length() - 1]);
-        t = t.substr(0, t.length() - 1);
-        x += t;
+    cin >> x;
+    cin >> q;
+    int n = x.length();
+    //? COW -> COW WCO -> COWWCO OCOWWC
+    while (n < q) {
+        i = n;
+        while (q > i) i *= 2;
+        i /= 2;
+        q -= (1 + i);
+        if (q == 0) q = i;
     }
-    // cout << x << endl;
-    cout << x[n - 1];
+    cout << x[q - 1];
     return 0;
 }
