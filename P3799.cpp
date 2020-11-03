1987fd204d13b4bc3e4f5d39cb39413cc47a7684
AMIRIOX <wumingyun2120@outlook.com>
2020-11-03 19:05:32 +0800

    </> P3799 排列组合+暴力枚举

--- P3799.cpp
+++ P3799.cpp
@@ -4,29 +4,29 @@
 #define INF 9223372036854775800LL
 using namespace std;
 const int maxn = 1e6 + 1;
-const int mod = 1e9 + 7;
-int n, have[maxn], minx = INF, maxx = -INF, ans, anst;
+const int mod = (1e9 + 7);
+int n, have[maxn], maxx = -INF, ans;
+int C2(int x) { return ((x * x - x) / 2) % mod; }
 signed main() {
+    int x;
     cin >> n;
     for (int i = 1; i <= n; i++) {
-        int x;
         cin >> x;
-        if (x < minx) minx = x;
         if (x > maxx) maxx = x;
         have[x]++;
     }
     for (int i = 1; i <= maxx; i++) {
         // // i=i=(j+k)
-        //* (i+j)=x1=x2, have[x]>=2
+        //* (i+j)=x1=x2
         for (int j = i; j <= maxx; j++) {
-            if (i == j) {
-                anst++;
-                continue;
-            }
-            if(have[i+j]>=2) ans++;
+            if (i == j)
+                ans = (ans + C2(have[i]) * C2(have[i + j])) % mod;
+            else
+                ans = (ans + C2(have[i]) * C2(have[j]) % mod * C2(have[i + j]) %
+                                 mod) %
+                      mod;
         }
     }
-    ans+=(anst/2);
-    cout << ans % mod << endl;
+    cout << ans << endl;
     return 0;
 }
