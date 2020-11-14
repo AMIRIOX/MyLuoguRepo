069a238e97aa6e4219649ab36f37db03eea92766
AMIRIOX <wumingyun2120@outlook.com>
2020-11-14 22:39:48 +0800

    弓箭复杂度优化的01背包, 把i维度舍去, 反向遍历

--- template#01backpack.cpp
+++ template#01backpack.cpp
@@ -2,18 +2,32 @@
 #include <cmath>
 using namespace std;
 const int maxn = 1e3+1;
-int v[maxn],w[maxn],n,weight,f[maxn][maxn];
+int v[maxn],w[maxn],n,weight,f[maxn][maxn],dp[maxn];
 int main() {
     cin >> weight >> n;
     for(int i=1;i<=n;i++) {
         cin >> w[i] >> v[i];
     }
-    for(int i=1;i<=n;i++) {
-        for(int j=0; j<=weight; j++) {
-            if(j>=w[i]) f[i][j] = max(f[i-1][j],f[i-1][j-w[i]]+v[i]);
-            else f[i][j] = f[i-1][j];
+
+    // usually
+    if(1==0) { //! will nerver execute
+        for(int i=1;i<=n;i++) {
+            for(int j=0; j<=weight; j++) {
+                if(j>=w[i]) f[i][j] = max(f[i-1][j],f[i-1][j-w[i]]+v[i]);
+                else f[i][j] = f[i-1][j];
+            }
         }
     }
-    cout << f[n][weight];
+
+    // special
+    for(int i=1;i<=n;i++) {
+        for(int j=weight; j>=0; j--) {
+            if(w[i]<=j) dp[j] = max(dp[j],dp[j-w[i]]+v[i]);
+            if(i==n && j==0) break;
+        }
+    }
+
+    // cout << f[n][weight];
+    cout << dp[weight];
     return 0;
 }
