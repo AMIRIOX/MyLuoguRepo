971944a67757f52a31ecef17738bfa9e59e5158e
AMIRIOX <wumingyun2120@outlook.com>
2020-11-23 19:04:07 +0800

    P5638 原思路, 枚举跳过的区间然后计算和, AC了一部分点, TLE了一部分

--- P5638.cpp
+++ P5638.cpp
@@ -10,9 +10,9 @@ int a[maxn],fdis[maxn];
 int ans=inf;
 signed main() {
   scanf("%lld %lld", &n, &k);
-  for(int i=2;i<=n;i++) {
+  for(int i=1;i<n;i++) {
     scanf("%lld", &a[i]);
-    a[i]+=a[i-1];
+    // a[i]+=a[i-1];
   }
   // // (1)__1__(2)__2__(3)__3__(4)__4__(5)
   // // (实际意义)
@@ -20,18 +20,20 @@ signed main() {
   // // i+1 -> i+k
   // // (1)____a[1]____(2)
   // // 计算1->n
-  // int sum = 0;
-  // for(int i=1;i<n;i++) {
-  //   sum+=a[i];
-  // }
-  // for(int i=1;i<n;i++) {
-  //     int insum=0;
-  //     for(int j=i;j<=i+k-1;j++) {
-  //       insum+=a[i];
-  //     }
-  //     if(sum>=insum) ans=min(ans,sum-insum);
-  // }
 
+  int sum = 0;
+  for(int i=1;i<n;i++) {
+    sum+=a[i];
+  }
+  for(int i=1;i<n;i++) {
+      int insum=0;
+      for(int j=i;j<=i+k-1;j++) {
+        insum+=a[j];
+      }
+      if(sum>=insum) ans=min(ans,sum-insum);
+  }
+
+  //
   // // 前缀和储存1->i, i->n
   // for(int i=1;i<n;i++) {
   //   fdis[i]=fdis[i-1]+a[i];
@@ -42,12 +44,15 @@ signed main() {
   // for(int i=1;i<n;i++) {
   //
   // }
-  for(int i=1;i<=n;i++) {
-    fdis[i]=a[i]+(a[n]-a[i+k]);
-  }
-  for(int i=1;i<=n;i++) {
-    ans=min(ans,fdis[i]);
-  }
-  printf("%lld", ans);
+  //
+  // for(int i=1;i<=n;i++) {
+  //   fdis[i]=a[i]+(a[n]-a[i+k]);
+  // }
+  // for(int i=1;i<=n;i++) {
+  //   ans=min(ans,fdis[i]);
+  // }
+  // printf("%lld", ans);
+
+  printf("%lld",ans);
   return 0;
 }
