dd1ebb7c6b14d1a68cf50bb42a26bbb024e21193
AMIRIOX <wumingyun2120@outlook.com>
2020-11-24 23:36:49 +0800

    update test

--- test.cpp
+++
@@ -1,9 +0,0 @@
-for(i=1;i<n;i++) {
-    for(int j=0;j<=w;j++) {
-        if(j>=w1[i]) {
-            dp_1[i][j]=max(dp_1[i-1][j],dp_1[i-1][j-w1[i]]+v[i]);
-        }else {
-            dp_1[i][j]=dp_1[i-1][j];
-        }
-    }
-}
