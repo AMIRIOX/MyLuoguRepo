61373bfc89f3d0a88b7d47c282d59f42cddbf2d2
AMIRIOX <wumingyun2120@outlook.com>
2020-12-23 12:43:23 +0800

    a water problem but I browse the answer :( 1. greedy: if sum is less than 0 then take sum's value to 0 today is terrible :(

--- P1115.cpp
+++ P1115.cpp
@@ -2,17 +2,16 @@
 #define int long long
 using namespace std;
 const int inf = 0x7fffffff;
-int n, a[100010], pre[100010], ans = -inf;
+int n, c, /*pre[100010], */ ans = -inf, sum;
 signed main() {
     cin >> n;
-    for(int i=1; i<=n; i++) cin >> a[i];
-    for(int i=1; i<=n; i++) {
-        pre[i] = pre[i-1]+a[i];
-    }
-    for(int i=1; i<n; i++) {
-        for(int j=i+1; j<=n; j++) {
-            // pre[j]-pre[i-1]
-            ans = max(ans, pre[j]-pre[i-1]);
+    for(int i=1; i<=n ;i++) {
+        cin >> c;
+        if(i==1) sum = ans = c;
+        else {
+            sum = sum>0?sum:0;
+            sum += c;
+            ans = max(sum, ans);
         }
     }
     cout << ans << endl;
