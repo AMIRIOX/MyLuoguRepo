fec5ab821ef3cb0a6800c8e7564c4a8836230c10
AMIRIOX <wumingyun2120@outlook.com>
2021-01-24 17:55:07 +0800

    [mess] modify some templates and problems

---
+++ CF896C.cpp
@@ -0,0 +1,134 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <vector>
+#define IT set<node>::iterator
+using namespace std;
+typedef long long ll;
+const ll maxn = 1e5 + 10;
+ll n, m, seed, vmax;
+ll a[maxn];
+ll qpow(ll a, ll ni, ll mod) {
+    ll ans = 1;
+    a = a % mod;
+    while (ni) {
+        if (ni & 1)
+            ans = ans * a % mod;
+        a = a * a % mod;
+        ni >>= 1;
+    }
+    return ans;
+}
+struct node {
+    int l, r;
+    mutable ll v;
+    node(int _l, int _r = -1, ll _v = 0) : l(_l), r(_r), v(_v) {}
+    bool operator<(const node& o) const { return l < o.l; }
+};
+set<node> s;
+IT split(int pos) {
+    IT itr = s.lower_bound(node(pos));
+    if (itr != s.end() && itr->l == pos)
+        return itr;
+    --itr;
+    int OL = itr->l, OR = itr->r;
+    ll OV = itr->v;
+    s.erase(itr);
+    s.insert(node(OL, pos - 1, OV));
+    return s.insert(node(pos, OR, OV)).first;
+}
+void assign(int nl, int nr, ll val = 0) {
+    IT itr = split(nr + 1), itl = split(nl);
+    s.erase(itl, itr);
+    s.insert(node(nl, nr, val));
+}
+void add(int nl, int nr, ll val = 1) {
+    IT itr = split(nr + 1), itl = split(nl);
+    while (itl != itr)
+        itl->v += val, itl++;
+}
+ll kth(int nl, int nr, int k) {
+    vector<pair<ll, int> > vp;
+    IT itr = split(nr + 1), itl = split(nl);
+    // vp.clear();
+    while (itl != itr)
+        vp.push_back(pair<ll, int>(itl->v, itl->r - itl->l + 1)),
+            itl++;  // TODO
+    sort(vp.begin(), vp.end());
+    for (vector<pair<ll, int> >::iterator it = vp.begin(); it != vp.end();
+         ++it) {
+        k -= it->second;
+        if (k <= 0)
+            return it->first;
+    }
+    return -1ll;
+}
+ll sum(int nl, int nr, int x, int y) {
+    ll res = 0;
+    IT itr = split(nr + 1), itl = split(nl);
+    while (itl != itr) {
+        res = (res + (itl->r - itl->l + 1) * qpow(itl->v, x, y)) % y;
+        // res %= y;
+        itl++;
+    }
+    return res;
+}
+ll rnd() {
+    ll ret = seed;
+    seed = (seed * 7 + 13) % 1000000007;
+    return ret;
+}
+int main() {
+    scanf("%lld %lld %lld %lld", &n, &m, &seed, &vmax);
+    for (int i = 1; i <= n; i++) {
+        a[i] = (rnd() % vmax) + 1;
+        s.insert(node(i, i, a[i]));
+    }
+    s.insert(node(n + 1, n + 1, 0));
+    // cout << "input ok" << endl;
+    for (int i = 1; i <= m; i++) {
+        /*
+            op = (rnd() mod 4) + 1
+            l = (rnd() mod n) + 1
+            r = (rnd() mod n) + 1
+
+            if (l > r):
+                 swap(l, r)
+
+            if (op == 3):
+                x = (rnd() mod (r - l + 1)) + 1
+            else:
+                x = (rnd() mod vmax) + 1
+
+            if (op == 4):
+                y = (rnd() mod vmax) + 1
+        */
+        ll op = (rnd() % 4) + 1;
+        ll L = (rnd() % n) + 1;
+        ll R = (rnd() % n) + 1;
+        ll x, y;
+        if (L > R)
+            swap(L, R);
+        if (op == 3)
+            x = (rnd() % (R - L + 1)) + 1;
+        else
+            x = (rnd() % vmax) + 1;
+
+        if (op == 4)
+            y = (rnd() % vmax) + 1;
+
+        // cout << op << ": ";
+        // cout.flush();
+        if (op == 1) {
+            add(L, R, x);
+        } else if (op == 2) {
+            assign(L, R, x);
+        } else if (op == 3) {
+            printf("%lld\n", kth(L, R, x));
+        } else if (op == 4) {
+            printf("%lld\n", sum(L, R, x, y));
+        }
+    }
+    return 0;
+}
--- template#dijkstra.cpp
+++ template#dijkstra.cpp
@@ -1,90 +1,133 @@
-#include <cstdio>
 #include <iostream>
-#include <queue>
-#define re register
 using namespace std;
-const int maxn = 100;
-const int INF = 0x7fffffff;
-int G[maxn][maxn], n, m, s, dis[maxn], book[maxn];
-// // void dfs(int u) {
-// //     int minx = INF;
-// //     for (int i = 1; i <= n; i++) {
-// //         if (book[i]) continue;
-// //         minx = min(minx, dis[i]);
-// //     }
-// //     for (int i = 1; i <= n; i++) {
-// //         if (G[u][i]) {
-// //             if (dis[i] > dis[u] + G[u][i]) dis[i] = dis[u] + G[u][i];
-// //         }
-// //     }
-// //     book[u] = 1;
-// // }
-// // queue<int> q;
-// // q.push(minfunc());
-// // while (!q.empty()) {
-// //     int i = q.front();
-// //     q.pop();
-// //     for (int j = 1; j <= n; j++) {
-// //         if (G[i][j]) {
-// //             if (dis[j] > dis[i] + G[i][j]) dis[j] = dis[i] + G[i][j];
-// //             q.push(j);
-// //         }
-// //     }
-// //     book[i] = 1;
-// // }
-
+const int maxn = 1e4+10;
+const int inf = 0x7fffffff;
+int G[maxn][maxn], dis[maxn], book[maxn];
+int n,m,s;
 int minfunc() {
-    int minx = INF, minu;
-    for (int i = 1; i <= n; i++) {
-        if (book[i]) continue;
-        if (dis[i] < minx) {
-            minx = dis[i];
-            minu = i;
-        }
-    }
-    return minu;
+	int minu,minv=inf;
+	for(int i=1;i<=n;i++) {
+		if(book[i]) continue;
+		if(dis[i]<minv) {
+			minv = dis[i];
+			minu=i;
+		}
+	}
+	return minu;
 }
 int main() {
-    // input
-    cin >> n >> m >> s;
-    // 注意, 如果有零权的边, 需要将G数组填为INF
-    for (int i = 1; i <= m; i++) {
-        int x, y, v;
-        cin >> x >> y >> v;
-        G[x][y] = v;  //如果有重边 使用min(v, G[x][y]);
-    }
-    //? 设置所有能直接到达1的点的位置的dis[i]为G[1][i]
-    //? 设置不能直接到打1的点的dis[i]为INF, 设置book[1]为1
-    for (int i = 1; i <= n; i++) {
-        if (G[s][i])
-            dis[i] = G[s][i];
-        else
-            dis[i] = INF;
-        if (i == s) dis[i] = 0;
-    }
-    //? 找到一个最小的dis[i], 枚举i点的每条出边(i,j),
-    //? 如果dis[j]>dis[i]+G[i][j],则更新dis[j]=~
-    //? 最后设book[i]为1
-    for (int y = 1; y < n; y++) {
-        // bool isok = 1;
-        // for (int i = 1; i <= n; i++) {
-        //     if (!book[i]) isok = 0;
-        // }
-        // if (isok) break;
-        int i = minfunc();
-        for (int j = 1; j <= n; j++) {
-            if (G[i][j] && i != j) {
-                if (dis[j] > dis[i] + G[i][j]) dis[j] = dis[i] + G[i][j];
-            }
-        }
-        book[i] = 1;
-    }
-
-    //? 再次从剩下的边(book[i]==0)找出最小的 然后重复上述操作
-    //? 直到所有边都访问完了
-    for (int i = 1; i <= n; i++) {
-        // cout << "The distance between 1 and " << i << " is " << dis[i] << endl;
-        cout << dis[i] << " ";
-    }
-    return 0;
+	cin >> n >> m >> s;
+	for(int i=1;i<=n;i++) 
+		for(int j=1;j<=n;j++)
+			if(i==j) G[i][j]=0;
+			else G[i][j]=inf;
+	for(int i=1;i<=m;i++) {
+		int x,y,v;
+		cin >> x >> y >> v;
+		G[x][y] = min(G[x][y],v);
+	}
+	for(int i=1;i<=n;i++) dis[i] = G[s][i];
+	for(int y=1;y<=n-1;y++) {
+		int u = minfunc();
+		for(int i=1;i<=n;i++) {
+			if(G[u][i]!=inf) {
+				if(dis[i]>dis[u]+G[u][i]) 
+					dis[i]=dis[u]+G[u][i];
+			}
+		}
+		book[u]=1;
+	}
+	for(int i=1;i<=n;i++) cout << dis[i] << " ";
+	return 0;
 }
+
+// #include <cstdio>
+// #include <iostream>
+// #include <queue>
+// #define re register
+// using namespace std;
+// const int maxn = 100;
+// const int INF = 0x7fffffff;
+// int G[maxn][maxn], n, m, s, dis[maxn], book[maxn];
+// // // void dfs(int u) {
+// // //     int minx = INF;
+// // //     for (int i = 1; i <= n; i++) {
+// // //         if (book[i]) continue;
+// // //         minx = min(minx, dis[i]);
+// // //     }
+// // //     for (int i = 1; i <= n; i++) {
+// // //         if (G[u][i]) {
+// // //             if (dis[i] > dis[u] + G[u][i]) dis[i] = dis[u] + G[u][i];
+// // //         }
+// // //     }
+// // //     book[u] = 1;
+// // // }
+// // // queue<int> q;
+// // // q.push(minfunc());
+// // // while (!q.empty()) {
+// // //     int i = q.front();
+// // //     q.pop();
+// // //     for (int j = 1; j <= n; j++) {
+// // //         if (G[i][j]) {
+// // //             if (dis[j] > dis[i] + G[i][j]) dis[j] = dis[i] + G[i][j];
+// // //             q.push(j);
+// // //         }
+// // //     }
+// // //     book[i] = 1;
+// // // }
+
+// int minfunc() {
+//     int minx = INF, minu;
+//     for (int i = 1; i <= n; i++) {
+//         if (book[i]) continue;
+//         if (dis[i] < minx) {
+//             minx = dis[i];
+//             minu = i;
+//         }
+//     }
+//     return minu;
+// }
+// int main() {
+//     // input
+//     cin >> n >> m >> s;
+//     // 注意, 如果有零权的边, 需要将G数组填为INF
+//     for (int i = 1; i <= m; i++) {
+//         int x, y, v;
+//         cin >> x >> y >> v;
+//         G[x][y] = v;  //如果有重边 使用min(v, G[x][y]);
+//     }
+//     //? 设置所有能直接到达1的点的位置的dis[i]为G[1][i]
+//     //? 设置不能直接到打1的点的dis[i]为INF, 设置book[1]为1
+//     for (int i = 1; i <= n; i++) {
+//         if (G[s][i])
+//             dis[i] = G[s][i];
+//         else
+//             dis[i] = INF;
+//         if (i == s) dis[i] = 0;
+//     }
+//     //? 找到一个最小的dis[i], 枚举i点的每条出边(i,j),
+//     //? 如果dis[j]>dis[i]+G[i][j],则更新dis[j]=~
+//     //? 最后设book[i]为1
+//     for (int y = 1; y < n; y++) {
+//         // bool isok = 1;
+//         // for (int i = 1; i <= n; i++) {
+//         //     if (!book[i]) isok = 0;
+//         // }
+//         // if (isok) break;
+//         int i = minfunc();
+//         for (int j = 1; j <= n; j++) {
+//             if (G[i][j] && i != j) {
+//                 if (dis[j] > dis[i] + G[i][j]) dis[j] = dis[i] + G[i][j];
+//             }
+//         }
+//         book[i] = 1;
+//     }
+
+//     //? 再次从剩下的边(book[i]==0)找出最小的 然后重复上述操作
+//     //? 直到所有边都访问完了
+//     for (int i = 1; i <= n; i++) {
+//         // cout << "The distance between 1 and " << i << " is " << dis[i] << endl;
+//         cout << dis[i] << " ";
+//     }
+//     return 0;
+// }
