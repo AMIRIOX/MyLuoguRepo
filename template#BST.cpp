a7a72e74d8125bb41dae9ee8229ddfae60393355
AMIRIOX <wumingyun2120@outlook.com>
2021-02-17 18:41:47 +0800

    [review] review of BST

--- template#BST.cpp
+++ template#BST.cpp
@@ -1,77 +1,75 @@
+// Binary Search Tree
+// 二叉搜索树
 #include <cstdio>
 #include <iostream>
 using namespace std;
 const int maxn = 5e5 + 10;
-int val[maxn], ls[maxn], rs[maxn];
-int cnt[maxn], siz[maxn], sum;
-
-void print(int o) {
-    if (!o)
+int cnt[maxn], ls[maxn], rs[maxn];
+int siz[maxn], val[maxn], sum;
+void print(int r) {
+    if (!r)
         return;
-
-    print(ls[o]);
-    for (int i = 1; i <= cnt[o]; i++)
-        printf("%d ", val[o]);
-    printf("\n");
-    print(rs[o]);
-}
-int getmin(int o) {
-    if (!ls[o])
-        return val[o];
-    return getmin(ls[o]);
+    print(ls[r]);
+    for (int i = 1; i <= cnt[r]; i++)
+        printf("%d ", val[r]);
+    print(rs[r]);
 }
 int getmax(int o) {
     if (!rs[o])
         return val[o];
     return getmax(rs[o]);
 }
-void insert(int& o, int v) {
-    if (!o) {
-        val[o = ++sum] = v;
-        siz[o] = cnt[o] = 1;
+int getmin(int o) {
+    if (!ls[o])
+        return val[o];
+    return getmin(ls[o]);
+}
+void insert(int& rt, int vl) {
+    if (!rt) {
+        val[rt = ++sum] = vl;
+        siz[rt] = cnt[rt] = 1;
         return;
     }
-    siz[o]++;
-    if (val[o] == v) {
-        cnt[o]++;
+    siz[rt]++;
+    if (val[rt] == vl) {
+        cnt[rt]++;
         return;
     }
-    if (v > val[o])
-        insert(rs[o], v);
-    if (v < val[o])
-        insert(ls[o], v);
+    if (vl > val[rt])
+        insert(rs[rt], vl);
+    else if (vl < val[rt])
+        insert(ls[rt], vl);
 }
 int delmin(int& o) {
     if (!ls[o]) {
         int u = o;
         o = rs[o];
         return u;
+    } else {
+        int u = delmin(ls[o]);
+        siz[o] -= cnt[u];
+        return u;
     }
-    int u = delmin(ls[o]);
-    siz[o] -= cnt[u];
-    return u;
 }
-
 void del(int& o, int v) {
     siz[o]--;
     if (val[o] == v) {
         if (cnt[o] > 1) {
             cnt[o]--;
             return;
-        } else {
-            if (ls[o] && rs[o])
-                o = delmin(rs[o]);
-            else
-                o = ls[o] | rs[o];
-            return;
         }
+        // 判断是否二胎
+        if (ls[o] && rs[o])
+            o = delmin(rs[o]);
+        else
+            o = (ls[o] | rs[o]);
+        return;
     }
     if (v > val[o])
         del(rs[o], v);
     if (v < val[o])
         del(ls[o], v);
 }
-
 int rnk(int o, int v) {
     if (val[o] == v)
         return siz[ls[o]] + 1;
@@ -86,4 +84,8 @@ int kth(int o, int k) {
     if (siz[ls[o]] < k - cnt[o])
         return kth(rs[o], k - cnt[o] - siz[ls[o]]);
     return val[o];
+}
+int main() {
+    //TODO
+    return 0;
 }
