443c1a8ab03883fbf8581f6046b5f01c2930f3d1
AMIRIOX <wumingyun2120@outlook.com>
2021-02-22 17:52:35 +0800

    [learn] 判1能连到的负环 | bellman-ford | TODO 写SPFA判负环

--- P3385.cpp
+++ P3385.cpp
@@ -1,60 +1,3 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// const int maxn = 1e8+10;
-// int n, m, s;
-// struct edge
-// {
-//     int self, to, w, nxt;
-// } g[maxn];
-// int tot, head[maxn];
-// void addEdge(int u, int to, int w)
-// {
-//     g[++tot].to = to;
-//     g[tot].self = u;
-//     g[tot].w = w;
-//     g[tot].nxt = head[u];
-//     head[u] = tot;
-// }
-// int dis[maxn];
-// bool bellman_ford()
-// {
-//     memset(dis, 0x3f, sizeof(dis));
-//     dis[s] = 0;
-//     for (int i = 1; i < n; i++)
-//     {
-//         for (int cnt = 1; cnt <= tot; cnt++)
-//         {
-//             if (dis[g[cnt].to] > dis[g[cnt].self] + g[cnt].w)
-//             {
-//                 dis[g[cnt].to] = dis[g[cnt].self] + g[cnt].w;
-//             }
-//         }
-//     }
-//     for (int i = 1; i <= tot; i++)
-//     {
-//         if (dis[g[i].to] > dis[g[i].self] + g[i].w)
-//             return true;
-//     }
-//     return false; //TODO
-// }
-// int main()
-// {
-//     scanf("%d %d %d", &n, &m, &s);
-//     for (int i = 1; i <= m; i++)
-//     {
-//         int x, y, v;
-//         scanf("%d %d %d", &x, &y, &v);
-//         addEdge(x, y, v);
-//     }
-//     if(bellman_ford()) printf("YES\n");
-//     for (int i = 1; i <= n; i++)
-//     {
-//         printf("%d ", dis[i] == 0x3f3f3f3f ? 0x7fffffff : dis[i]);
-//     }
-//     return 0;
-// }
-
-#ifdef BELLMAN_FORD_METHOD
 #include <cstdio>
 #include <cstring>
 #include <iostream>
@@ -111,5 +54,3 @@ int main() {
     }
     return 0;
 }
-#endif
-
