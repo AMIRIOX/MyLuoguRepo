9372c3a38de60312035256b2a973ceda7fab082b
AMIRIOX <wumingyun2120@outlook.com>
2021-07-29 20:41:18 +0800

    [P2058] 队列水题

--- P2058.cpp
+++ P2058.cpp
@@ -1,24 +1,37 @@
-#include <iostream>
 #include <algorithm>
-#include <queue>
 #include <cstdio>
+#include <iostream>
+#include <queue>
 using namespace std;
-const int pw=86400;
-struct po{
+const int pw = 86400;
+const int maxn=3e5+10;
+struct po {
     int time, nation;
-}pos[12200];
-int n, cnt;
+} pos[maxn];
+int n, cnt, nations[maxn], ntscnt;
+queue<po> q;
 signed main() {
     scanf("%d", &n);
-    for(int i=1;i<=n;i++) {
-        int ti,ki;
+    for (int i = 1; i <= n; i++) {
+        int ti, ki;
         scanf("%d %d", &ti, &ki);
-        for(int j=1;j<=ki;j++) {
+        for (int j = 1; j <= ki; j++) {
             int xi;
             scanf("%d", &xi);
-            pos[++cnt].time=ti;
-            pos[cnt].nation=xi;
+            pos[++cnt].time = ti;
+            pos[cnt].nation = xi;
+            q.push(pos[cnt]);
+            if (!nations[xi])
+                ntscnt++;
+            nations[xi]++;
         }
+        while (!q.empty() && ti - q.front().time >= pw) {
+            nations[q.front().nation]--;
+            if (!nations[q.front().nation])
+                ntscnt--;
+            q.pop();
+        }
+        printf("%d\n", ntscnt);
     }
     return 0;
 }
