4904c3d302b043ae129510bc4b83e8e78e020c52
AMIRIOX <wumingyun2120@outlook.com>
2024-07-19 20:37:34 +0800

    [contest/off] Codeforces 956 Div2 (2/6), C in plan

---
+++ rates/offline/CF#956-div2/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int k = 0; cin >> k;
+    for(int i = 1; i <= k; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1; cin >> t;
+    while(t--) solve();
+    return 0;
+}
