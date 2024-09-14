45fd0505f8275f9dd97c6a0aaa494a6ed02975e3
AMIRIOX <wumingyun2120@outlook.com>
2024-09-15 07:15:55 +0800

    [Div2-972] B1...

--- rates/CF#972-div2/B1.cpp
+++ rates/CF#972-div2/B1.cpp
@@ -6,10 +6,10 @@ void solve() {
     cin >> n >> m >> q >> t1 >> t2 >> d1;
     if(d1 == t1 || d1 == t2) cout << 0 << endl;
     else if(d1 > min(t1, t2) && d1 < max(t1,t2)) {
-        int tmp = (max(t1, t2) - d1) - (d1 - min(t1, t2));
-        int tmp2 = (max(t1, t2) - d1) - (d1 - min(t1, t2) + 1);
-        if(tmp % 2 == 0) cout << tmp / 2 + (d1 - min(t1, t2)) << endl;
-        else cout << tmp2 / 2 + (d1 - min(t1, t2)) << endl;
+        // min(t1, t2)    d1    max(t1, t2)
+        int cnt = 0;
+        if(t1 > t2) swap(t1, t2);
+        cout << (t2 - t1)/2 << endl;
     }
         /*cout << min(d1 - min(t1, t2), max(t1, t2) - d1) << endl;*/
     else if(d1 < min(t1, t2)) /*if(min(t1, t2) - d1 < d1 - 1) cout << d1 - 1 << endl; else*/ cout << min(t1, t2) - d1 + (d1 - 1) << endl;
