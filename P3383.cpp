4c53fdbb7151d88b6fe3cbbb9c1354c99d98da5a
AMIRIOX <wumingyun2120@outlook.com>
2024-11-22 11:40:45 +0800

    [P3383] Euler Sieve Template

--- P3383.cpp
+++ P3383.cpp
@@ -1,48 +1,40 @@
-#include <algorithm>
-#include <cstdio>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-const int MAXN = 1e8 + 10;
-int a[MAXN], b[MAXN];
+
+const int maxn = 1e8 + 10;
+
 inline int read() {
     int val = 0, flg = 1;
-    char ch = (char)getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') flg = 0;
-        ch = (char)getchar();
+    char ch = (char) getchar();
+
+    while(ch < '0' || ch > '9') {
+        if(ch == '-') flg = 0;
+        ch = (char) getchar();
     }
-    while (ch >= '0' && ch <= '9') {
+    while(ch >= '0' && ch <= '9') {
         val = (val << 1) + (val << 3) + (ch ^ 48);
-        ch = (char)getchar();
+        ch = (char) getchar();
     }
-    return (flg ? val : (~val) + 1);
+    
+    return flg ? val : ~(val - 1);
 }
-int main() {
-    int n, q, cnt = 0;
-    n = read();
-    q = read();
-    a[0] = a[1] = 1;
-    for (int i = 2; i <= n; i++) {
-        if (a[i] == 0) {
-            b[cnt++] = i;
-            for (int j = i << 1; j <= n; j += i) {
-                a[j] = 1;
-            }
+
+vector<int> pri;
+bool np[maxn];
+int n, q;
+signed main() {
+    n = read(); q = read();
+    for(int i = 2; i <= n; ++i) {
+        if(!np[i]) pri.push_back(i);
+        for(int prij: pri) {
+            if(i * prij > n) break;
+            np[i * prij] = true;
+            if(i % prij == 0) break;
         }
     }
-    for (int i = 1; i <= q; i++) {
-        int x = read();
-        // for (int i = 0; i <= n; i++) {
-        //     if (a[i] == 0) x--;
-        //     if (x == 0) {
-        //         cout << i << endl;
-        //         break;
-        //     }
-        // }
-        cout << b[x-1] << endl;
+    for(int i = 1; i <= q; i++) {
+        int v = read();
+        printf("%d\n", pri[v - 1]);
     }
-    // for(int i=0;i<n;i++) {
-    //     cout << b[i] << ' ';
-    // }
     return 0;
 }
