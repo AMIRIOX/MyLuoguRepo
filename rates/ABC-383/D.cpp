f509283c99374610d8d7d032c994ca1f561ba3e1
AMIRIOX <wumingyun2120@outlook.com>
2024-12-08 21:51:25 +0800

    [contest/off] a^2 * b^2 或 a^8 (a,b质数) 恰好有九个正因数

--- rates/ABC-383/D.cpp
+++ rates/ABC-383/D.cpp
@@ -3,7 +3,32 @@ using namespace std;
 
 using ll = long long;
 
+const ll maxn = 2e6 + 10;
+bool np[maxn];
 signed main() {
-
+    cin.tie(0) -> sync_with_stdio(false);
+    np[0] = np[1] = true;
+    ll n; cin >> n;
+    vector<ll> prime;
+    for(ll i = 2; i <= ll(2e6); i++) {
+        if(!np[i]) {
+            prime.push_back(i);
+            for(ll j = i * i; j <= ll(2e6); j += i){
+                np[j] = true;
+            }
+        }
+    }
+    // a^2 * b^2
+    ll ans = 0;
+    for(size_t i = 0; i < prime.size() - 1; i++) {
+        ll t = prime[i];
+        if(t * t * t * t > n) break;
+        if(t <= 100 && t * t * t * t * t * t * t * t <= n) ans++;
+        for(size_t j = i + 1; j < prime.size(); j++) {
+            if(prime[i] * prime[i] * prime[j] * prime[j] <= n) ans++;
+            else break;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
