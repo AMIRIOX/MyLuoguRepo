90318f89bf2e9f24bf89b07bc72c8da6f65fd884
AMIRIOX <wumingyun2120@outlook.com>
2024-12-26 20:04:46 +0800

    [contest/vp] Codeforces 995 Div3 E sol_1

--- rates/offline/CF#995-div3/E.cpp
+++ rates/offline/CF#995-div3/E.cpp
@@ -1,22 +1,31 @@
+/* WA
+#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
 struct guest {
-    int a;
-    int b;
+    ll a;
+    ll b;
     bool operator<(const guest &g) {
-        if (this->a != g.a)
-            return this->a > g.a;
-        else
+        if (this->b != g.b)
             return this->b > g.b;
+        else
+            return this->a > g.a;
     }
 };
 ll n, k;
-bool check(vector<ll> &a, vector<ll> &b, ll pris, ll pri) {
-    // pri <= ai        tree++
-    // ai < pri <= bi  tree++, neg++
-    int neg = 0, tree = 0;
+ll check(vector<guest> &gst, ll price) {
+    ll trees = upper_bound(gst.begin(), gst.end(), price, [](const ll &prc, const guest &g) {
+        return g.b < prc;
+            }) - gst.begin();
+    ll neg = upper_bound(gst.begin(), gst.end(), price, [](const ll &prc, const guest &g) {
+        return g.a < prc;
+            }) - gst.begin();
+    neg = trees - neg;
+    cout << price << " " << trees << " " << neg << endl;
+    if(neg <= k) return price * trees;
+    else return -1;
 }
 
 void solve() {
@@ -27,6 +36,12 @@ void solve() {
     for (int i = 0; i < n; i++)
         cin >> gst[i].b;
     sort(gst.begin(), gst.end());
+    ll ans = -1;
+    for(int i = 0; i < n; i++) {
+        ans = max(ans, check(gst, gst[i].a));
+        ans = max(ans, check(gst, gst[i].b));
+    }
+    cout << ans << '\n';
 }
 
 signed main() {
@@ -36,4 +51,40 @@ signed main() {
     while (tt--)
         solve();
     return 0;
+}
+*/
+
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n), b(n);
+    for(auto &i : a) cin >> i;
+    for(auto &i : b) cin >> i;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    
+    ll ans = -1;
+    auto check = [&](ll price) {
+        ll neg = lower_bound(a.begin(), a.end(), price) - a.begin();
+        neg -= lower_bound(b.begin(), b.end(), price) - b.begin();
+        ll tot = lower_bound(b.begin(), b.end(), price) - b.begin();
+        tot = n - tot;
+        if(neg <= k) return price * tot;
+        else return -1ll;
+    };
+    for(int i = 0; i < n; i++) {
+        ans = max(ans, check(a[i]));
+        ans = max(ans, check(b[i]));
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    int tt = 1; cin >> tt;
+    while(tt--) solve();
 }
